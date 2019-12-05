289d94d1dfaae39cbb258a38226eae07d8314fd4
oredola <oredolaolamide@gmail.com>
2019-12-05 17:01:28 +0100

    Added Display component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,18 @@
 import React, {useState } from 'react';
 import './App.css';
+import Display from './Display';
+import Dashboard from './Dashboard';
 
 function App() {
   const [count, setCount] = useState({
     strikes: 0,
     balls: 0
   });
-  
+
   return (
     <div className="App">
-      
+      <Display count={count} />
+      <Dashboard setCount={setCount} />
     </div>
   );
 }
--- my-app/src/Dashboard.js
+++ my-app/src/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Display() {
+export default function Dashboa() {
   return "lo";
 }
